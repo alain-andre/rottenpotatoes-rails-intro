56720352d7a5ad0891152348b85b0b59cbdced34
Alain ANDRE <dev@alain-andre.fr>
2017-06-20 13:33:44 +0000

    Ajout du choix des notes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ActiveRecord::Base
   ## 
   # @return all ratings
   def self.get_ratings
-    Movie.select(:rating).distinct
+    Movie.select(:rating).distinct.map(&:rating)
   end
 end
